27fc2afd3aa02169afcb916bbe031800e3717839
Typhoon <test4codding@gmail.com>
2026-01-07 02:02:53 +0300

    Add random color and opacity buttons

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-const container = document.querySelector(".container");
 const DEFAULT_SIDE_LENGTH = 16;
+const container = document.querySelector(".container");
 const gridSizeButton = document.querySelector(".grid-size-btn");
+const randomColorButton = document.querySelector(".random-color-btn");
+const opacityButton = document.querySelector(".opacity-btn");
+let isRandomColorActive = false;
+let isOpacityActive = false;
+
+opacityButton.addEventListener("click", () => {
+  isOpacityActive = opacityButton.classList.toggle("btn-active");
+});
+
+randomColorButton.addEventListener("click", () => {
+  isRandomColorActive = randomColorButton.classList.toggle("btn-active");
+});
 
 gridSizeButton.addEventListener("click", () => {
   const userInput = prompt("Enter number of squares per side (2-100): ");
@@ -13,16 +25,30 @@ gridSizeButton.addEventListener("click", () => {
 function createGrid(sideLength) {
   container.replaceChildren();
   const containerWidth = container.clientWidth;
-  const pixelWidth = containerWidth / sideLength;
-  for (let index = 0; index < sideLength ** 2; index++) {
-    const newDiv = document.createElement("div");
-    newDiv.style.width = `${pixelWidth}px`;
-    newDiv.style.height = `${pixelWidth}px`;
-    newDiv.addEventListener("mouseover", () => {
-      newDiv.style.backgroundColor = "black";
+  const gridWidth = containerWidth / sideLength;
+  for (let i = 0; i < sideLength ** 2; i++) {
+    const grid = document.createElement("div");
+    grid.style.width = `${gridWidth}px`;
+    grid.style.height = `${gridWidth}px`;
+    grid.style.opacity = 0.1;
+    grid.addEventListener("mouseenter", () => {
+      if (isOpacityActive) {
+        let currentOpacity = parseFloat(grid.style.opacity);
+        grid.style.opacity = Math.min(currentOpacity + 0.1, 1);
+      }
+      isRandomColorActive
+        ? (grid.style.backgroundColor = getRandomColor())
+        : (grid.style.backgroundColor = "black");
     });
-    container.appendChild(newDiv);
+    container.appendChild(grid);
   }
+}
+
+function getRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
 }
 
 createGrid(DEFAULT_SIDE_LENGTH);
